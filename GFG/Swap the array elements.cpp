c5d9986eedd697f877709d39913ed229cd849011
Himanshu Dhingra <104208171+dhingrahimanshu@users.noreply.github.com>
2024-03-04 11:36:27 +0530

    arrays

---
+++ GFG/Swap the array elements.cpp
@@ -0,0 +1,9 @@
+class Solution{
+  public:
+    void swapElements(int arr[], int n){
+        if(n<=2)return;
+        for(int i= 0;i<n-2;i++){
+            swap(arr[i] , arr[i+2]);
+        }
+    }
+};
