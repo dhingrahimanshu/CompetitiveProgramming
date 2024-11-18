911b8425a7e34767b8991691e3616a04c54b4448
Himanshu Dhingra <104208171+dhingrahimanshu@users.noreply.github.com>
2024-11-18 23:30:48 +0530

    morris traversal

---
+++ LeetCode/MorrisTravesalInorder.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+
+        while (root) {
+            if (root->left) {
+                TreeNode * temp = root->left;
+
+                while (temp->right && temp->right != root) {
+                    temp = temp->right;
+                }
+
+                if (temp->right == root) { // active link
+                    ans.push_back(root->val);
+                    temp->right = NULL;
+                    root = root->right;
+                } else {
+                    temp->right = root;
+                    root = root->left;
+                }
+            } else {
+                ans.push_back(root->val);
+                root = root->right;
+            }
+        }
+
+
+        return ans;
+    }   
+};
